de7d169331ba8a254059ccb30ee5b3dd8a46db72
Matt Day <mattsday@gmail.com>
2022-03-30 21:52:30 +0100

    Project fix

--- crisps/init.go
+++ crisps/init.go
@@ -21,11 +21,11 @@ func InitProfiler(name string, version string) (err error) {
 	}
 	return err
 }
-func InitTracing(name string, version string) (tracer trace.Tracer, err error) {
+func InitTracing(name string, project string) (tracer trace.Tracer, err error) {
 	log.Println("Initialising Tracing")
 	var r = 10
 	for i := 0; i < r; i++ {
-		tracer, err = Tracer(name, version)
+		tracer, err = Tracer(name, project)
 		if err == nil {
 			log.Println("Tracing loaded")
 			return tracer, nil
