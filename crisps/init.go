30e3669c1c246e7df50f845b7053c917b676bf0f
Matt Day <mattsday@gmail.com>
2022-03-30 17:54:21 +0100

    Added backoff for tracing and profiler

---
+++ crisps/init.go
@@ -0,0 +1,38 @@
+package crisps
+
+import (
+	"go.opentelemetry.io/otel/trace"
+	"log"
+	"time"
+)
+
+func InitProfiler(name string, version string) (err error) {
+	log.Println("Initialising Profiler")
+	var r = 10
+	for i := 0; i < r; i++ {
+		err = Profiler(name, version)
+		if err == nil {
+			log.Println("Profiler loaded")
+			return nil
+		} else {
+			log.Println("Profiler failed: "+err.Error()+" (attempt ", i, " of ", r, ")")
+			time.Sleep(2000 * time.Millisecond)
+		}
+	}
+	return err
+}
+func InitTracing(name string, version string) (tracer trace.Tracer, err error) {
+	log.Println("Initialising Tracing")
+	var r = 10
+	for i := 0; i < r; i++ {
+		tracer, err = Tracer(name, version)
+		if err == nil {
+			log.Println("Tracing loaded")
+			return tracer, nil
+		} else {
+			log.Println("Profiler failed: "+err.Error()+" (attempt ", i, " of ", r, ")")
+			time.Sleep(2000 * time.Millisecond)
+		}
+	}
+	return nil, err
+}
